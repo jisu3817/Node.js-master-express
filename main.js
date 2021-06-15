88ea1107c3051a6f6e5fa4966c860dcc464030db
김지수 <kimji@MacBook-Pro.local>
2021-06-15 14:31:41 +0900

    에러 처리

--- main.js
+++ main.js
@@ -32,26 +32,29 @@ app.get('/', (request, response) => {
   response.send(html);
 });
 
-app.get('/page/:pageId', (request, response) => {
+app.get('/page/:pageId', (request, response, next) => {
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-    var title = request.params.pageId;
-    var sanitizedTitle = sanitizeHtml(title);
-    var sanitizedDescription = sanitizeHtml(description, {
-      allowedTags:['h1']
-    });
-    var list = template.list(request.list);
-    var html = template.HTML(sanitizedTitle, list,
-      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-      ` <a href="/create">create</a>
-        <a href="/update/${sanitizedTitle}">update</a>
-        <form action="/delete_process" method="post">
+    if(err) {
+      next(err);
+    } else {
+        var title = request.params.pageId;
+        var sanitizedTitle = sanitizeHtml(title);
+        var sanitizedDescription = sanitizeHtml(description, {
+          allowedTags:['h1']
+        });
+       var list = template.list(request.list);
+       var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update/${sanitizedTitle}">update</a>
+          <form action="/delete_process" method="post">
           <input type="hidden" name="id" value="${sanitizedTitle}">
           <input type="submit" value="delete">
-        </form>`
-    );
-    response.writeHead(200);
-    response.end(html);
+          </form>`
+      );
+      response.send(html);
+    }
   });
 });
 
@@ -124,6 +127,15 @@ app.post('/delete_process', (request, response) => {
   fs.unlink(`data/${filteredId}`, (error) => {
     response.redirect('/');
   });
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 });
 
 app.listen(3000, () => console.log('3000번 포트로 서버 가동!'))
