b4cfd734297c6d22c04a322c042d39497ff9c2fa
김지수 <kimji@MacBook-Pro.local>
2021-06-15 15:47:30 +0900

    라우터로 파일 분리하기

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
 var bodyParser = require('body-parser');
 var compression = require('compression');
+var topicRouter = require('./routes/topic');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded ({extended: false}));
@@ -17,6 +18,7 @@ app.get('*', (request, response, next) => {
     next();
   });
 });
+app.use('/topic', topicRouter)
 
 app.get('/', (request, response) => {
   var title = 'Welcome'; 
@@ -30,103 +32,6 @@ app.get('/', (request, response) => {
     `<a href="/topic/create">create</a>`
   );
   response.send(html);
-});
-
-app.get('/topic/create', (request, response) => {
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/topic/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form> 
-  `, '');
-  response.send(html);
-});
-
-app.post('/topic/create_process', (request, response) => {
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
-    response.redirect(`/topic/${title}`);
-  });
-});
-
-app.get('/topic/update/:updateId', (request, response) => {
-  var filteredId = path.parse(request.params.updateId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-    var title = request.params.updateId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-      <form action="/topic/update_process" method="post">
-        <input type ="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p>
-          <textarea name="description" placeholder="description">${description}</textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-      `<a href="/topic/create">create</a> <a href="/topic/${title}">update</a>`
-    );  
-    response.send(html);
-  });
-});
-
-app.post('/topic/update_process', (request,response) => {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, (error) => {
-    fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
-      response.redirect(`/topic/${title}`);
-    });
-  });
-});
-
-app.post('/topic/ delete_process', (request, response) => {
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, (error) => {
-    response.redirect('/');
-  });
-});
-
-app.get('/topic/:pageId', (request, response, next) => {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-    if(err) {
-      next(err);
-    } else {
-        var title = request.params.pageId;
-        var sanitizedTitle = sanitizeHtml(title);
-        var sanitizedDescription = sanitizeHtml(description, {
-          allowedTags:['h1']
-        });
-       var list = template.list(request.list);
-       var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-          <a href="/topic/update/${sanitizedTitle}">update</a>
-          <form action="/topic/delete_process" method="post">
-          <input type="hidden" name="id" value="${sanitizedTitle}">
-          <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    }
-  });
 });
 
 app.use((req, res, next) => {
