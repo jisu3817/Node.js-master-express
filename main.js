ecd4f8eb125caf1cb61c0966b569d172f2b38682
김지수 <kimji@MacBook-Pro.local>
2021-06-15 15:57:28 +0900

    루트 디렉터리 파일 분리하기

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
 var compression = require('compression');
+var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
 
 app.use(express.static('public'));
@@ -18,21 +15,9 @@ app.get('*', (request, response, next) => {
     next();
   });
 });
-app.use('/topic', topicRouter)
 
-app.get('/', (request, response) => {
-  var title = 'Welcome'; 
-  var description = 'Hello, Node.js';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list,
-    `
-    <h2>${title}</h2>${description}
-    <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
-    `,
-    `<a href="/topic/create">create</a>`
-  );
-  response.send(html);
-});
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
 
 app.use((req, res, next) => {
   res.status(404).send('Sorry cant find that!');
